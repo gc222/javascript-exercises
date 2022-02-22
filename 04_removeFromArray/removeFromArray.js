44b4d7d4bc6bf368e8868c1f1e23b622b4c8a444
Gregory Chung <gregorychung21@gmail.com>
2022-02-22 15:25:04 +0000

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
+    // args: [arr], itemToBeRemoved, itemToBeRemoved
+    // arguments is an array-like object where length can be applied
+    let nums = arguments[0];
+    let items = Array.from(arguments);
+    let index;
+    for (let i = 1; i < arguments.length; i++) {
+        index = nums.indexOf(items[i]);
+        if (index > -1) {
+            nums.splice(index, 1);
+        }
 
+        
+    }
+
+    return nums;
 };
 
 // Do not edit below this line
