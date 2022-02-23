7af99ee5e9c2a5dddde117cdc61a6c739d7e410a
Gregory Chung <gregorychung21@gmail.com>
2022-02-23 01:32:53 +0000

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    if(typeof n !== "number" || typeof m !== "number") {
+        return "ERROR";
+    }
+    
+    if (n < 0 || m < 0) return "ERROR";
 
+    let low, high;
+    if (n > m) {
+        low = m;
+        high = n;
+    } else {
+        low = n;
+        high = m;
+    }
+ 
+    let sum = 0;
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
