acf99e1d62c16ef9d7f9a08254270fab21cea848
Gregory Chung <gregorychung21@gmail.com>
2022-02-23 02:09:06 +0000

    completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // easiest condition first i.e. if divisible by 400
+    if (year % 400 == 0) return true;
+
+    // more specific condition i.e. if divisible by 4 and
+    // not divisible by 100
+    if (year % 4 == 0 && year % 100 != 0) return true;
+    
+    // else false
+    return false;
 };
 
 // Do not edit below this line
