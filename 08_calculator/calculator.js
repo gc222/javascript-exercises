15afe840457d2294f194f7092992216be786b612
Gregory Chung <gregorychung21@gmail.com>
2022-02-23 14:14:09 +0000

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+  // array.foreach(function(item, index) {expression})
+  // you can utilise arrow function:
+  // array.foreach(item => expression)
+  // foreach(item, index) -> index doesn't need to be specified
+  nums.forEach(item => sum += item);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let res = 1;
+  nums.forEach(item => res *= item);
+  return res
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	// 3 * 2 * 1
+  // base case: when a == 1 then return 1 
+  // n * n-1 * n-1 ... 1
+  // a * factorial(a - 1)
+
+  if (a === 0) return 1;
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
