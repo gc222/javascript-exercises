240f27391c8fd0c9a34e58a0347cd5100e5924c5
Gregory Chung <gregorychung21@gmail.com>
2022-02-23 16:01:14 +0000

    half finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    // two pointer technique
+    
+    // strip punctuations
+    let st = s.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+    let string = st.toLowerCase();
 
+    let l = 0;
+    let r = string.length - 1;
+
+    while (l < r) {
+        if (string[l] )
+        if (string[l] !== string[r]) {
+            return false;
+        } else {
+            l++;
+            r--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
