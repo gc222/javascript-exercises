d12bf447ac7b91f3769f3bf457fb6a336fa40aec
Gregory Chung <gregorychung21@gmail.com>
2022-02-23 13:22:26 +0000

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5/9;
+  // round to nearest int -> 12.34 * 10 = 123.4 -> round
+  // -> 123 -> / 10 -> 12.3
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * 9/5 + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
